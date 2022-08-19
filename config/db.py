34d3460c1eed3ae2a44db546ce6c434a15e16bc4
IbrahimJessri <73992245+IbrahimJessri@users.noreply.github.com>
2022-08-19 04:09:05 +0300

    committed

--- config/db.py
+++ config/db.py
@@ -14,9 +14,13 @@ role_permission_collection = conn.AdServer2.role_permission
 served_ad_collection = conn.AdServer2.served_ad
 
 
-
 #gen.update_many(collection, {}, {"$set" : {"marketing_info.times_served" : 0} })
 
 
 #print(all_ads[0]["marketing_info"]["max_cpc"])
 
+
+
+# advertisement_collection.delete_many({})
+# interactive_advertisement_collection.delete_many({})
+# served_ad_collection.delete_many({})
