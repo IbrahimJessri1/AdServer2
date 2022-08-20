745e22811debe4b868873b4efefdfd604a4cae2b
IbrahimJessri <73992245+IbrahimJessri@users.noreply.github.com>
2022-08-20 17:10:16 +0300

    committed

--- config/db.py
+++ config/db.py
@@ -3,7 +3,7 @@ from pymongo import MongoClient
 from repositries import generics as gen
 
 
-conn = MongoClient("mongodb://localhost:27017/AdServer")
+conn = MongoClient("mongodb://localhost:27017/AdServer2")
 
 
 advertisement_collection = conn.AdServer2.advertisement
@@ -19,7 +19,9 @@ served_ad_collection = conn.AdServer2.served_ad
 
 #print(all_ads[0]["marketing_info"]["max_cpc"])
 
-
+# advertisement_collection.delete_many({})
+# interactive_advertisement_collection.delete_many({})
+# served_ad_collection.delete_many({})
 
 # advertisement_collection.delete_many({})
 # interactive_advertisement_collection.delete_many({})
